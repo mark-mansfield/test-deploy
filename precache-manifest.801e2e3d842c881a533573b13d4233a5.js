ad67c607fde840ab8783239b5a7a5e14156e9b2a
mark-mansfield <markgmansfield@gmail.com>
2019-07-24 09:06:51 +1000

    Updates

---
+++ precache-manifest.801e2e3d842c881a533573b13d4233a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26ef9c96e72e2ef4841718761eb43bde",
+    "url": "/test-deploy/index.html"
+  },
+  {
+    "revision": "2ae6741c6f1188b025e0",
+    "url": "/test-deploy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test-deploy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "2ae6741c6f1188b025e0",
+    "url": "/test-deploy/static/js/main.74c1ab4e.chunk.js"
+  },
+  {
+    "revision": "847df103da4b74cc84af",
+    "url": "/test-deploy/static/js/runtime~main.90bc13d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
